087428ac6f158c04074250450d71dbd9bb6f0ca0
Muthukumaran45 <muthukumarann4545@gmail.com>
2026-06-11 10:26:49 +0100

    api updated

--- src/api.jsx
+++ src/api.jsx
@@ -1,1 +1,2 @@
-export const BACKEND_API='https://adding-subtitle-backend-2.onrender.com'
+// export const BACKEND_API='https://adding-subtitle-backend-2.onrender.com'
+export const BACKEND_API='http://127.0.0.1:8000'
